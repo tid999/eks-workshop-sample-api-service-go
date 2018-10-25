41235b071f21d10b2244999a84dad9e84b882764
tid999 <30651510+tid999@users.noreply.github.com>
2018-10-25 13:44:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now"}
+		res := &response{Message: "Hello Jack's World - now"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
